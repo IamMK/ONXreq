677d8addc7ff52462c2ca6ab29deb66d3ba5bac9
IamMK <malk@int.pl>
2023-03-23 20:34:04 +0100

    Html part deleted, best implementation of this interface should be with js async query

--- task2/index.php
+++ task2/index.php
@@ -1,7 +1,7 @@
 <?php
 class TextInput
 {
-    protected $value = 'xd';
+    protected $value = '';
     public function add($text)
     {
         $this->value .= trim($text);
@@ -16,63 +16,8 @@ class NumericInput extends TextInput
 {
     public function add($text)
     {
-        $text = preg_replace('[0-9]', '', $text);
-        $this->value = trim($text);
+        if (is_numeric($text)) {
+            parent::add($text);
+        }
     }
 }
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <!-- meta etc. -->
-    <title>Document</title>
-    <style>
-        * {
-            box-sizing: border-box;
-        }
-
-        main {
-            padding: 20px;
-            width: 500px;
-            margin: auto;
-            text-align: center;
-        }
-
-        .userInterface {
-            margin: auto;
-            width: 500px;
-            display: flex;
-            flex-direction: column;
-            text-align: center;
-        }
-
-        .userInterface__el {
-            margin-top: 10px;
-        }
-    </style>
-</head>
-
-<body>
-
-    <!-- Beauty Site -->
-    <form class="userInterface">
-        <div class="userInterface__el"></div>
-        <label for="textinput">Text Input</label>
-        <input type="text" name="TextInput" id="textinput" />
-        <div class="userInterface__el"></div>
-        <label for="numericinput">Numeric Input</label>
-        <input type="number" name="NumericInput" id="numericinput">
-        <button class="userInterface__el" type="submit">Add</button>
-    </form>
-    <main>
-        <?php
-        $foo = new TextInput();
-
-        echo $foo->getValue();
-        ?>
-    </main>
-</body>
-
-</html>
