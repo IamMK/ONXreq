aa2b8d4ef767444961d9d2ee0e5c3db4fad33e1f
IamMK <malk@int.pl>
2023-03-23 20:21:29 +0100

    Little style, imitation of user Interface add

--- task2/index.php
+++ task2/index.php
@@ -1,12 +1,14 @@
 <?php
 class TextInput
 {
-    protected $value = '';
+    protected $value = 'xd';
     public function add($text)
     {
+        $this->value .= trim($text);
     }
-    public function getValue()
+    public function getValue(): string
     {
+        return $this->value;
     }
 }
 
@@ -14,6 +16,8 @@ class NumericInput extends TextInput
 {
     public function add($text)
     {
+        $text = preg_replace('[0-9]', '', $text);
+        $this->value = trim($text);
     }
 }
 ?>
@@ -25,6 +29,17 @@ class NumericInput extends TextInput
     <!-- meta etc. -->
     <title>Document</title>
     <style>
+        * {
+            box-sizing: border-box;
+        }
+
+        main {
+            padding: 20px;
+            width: 500px;
+            margin: auto;
+            text-align: center;
+        }
+
         .userInterface {
             margin: auto;
             width: 500px;
@@ -32,17 +47,32 @@ class NumericInput extends TextInput
             flex-direction: column;
             text-align: center;
         }
+
+        .userInterface__el {
+            margin-top: 10px;
+        }
     </style>
 </head>
 
 <body>
+
     <!-- Beauty Site -->
     <form class="userInterface">
+        <div class="userInterface__el"></div>
         <label for="textinput">Text Input</label>
         <input type="text" name="TextInput" id="textinput" />
+        <div class="userInterface__el"></div>
         <label for="numericinput">Numeric Input</label>
         <input type="number" name="NumericInput" id="numericinput">
+        <button class="userInterface__el" type="submit">Add</button>
     </form>
+    <main>
+        <?php
+        $foo = new TextInput();
+
+        echo $foo->getValue();
+        ?>
+    </main>
 </body>
 
 </html>
