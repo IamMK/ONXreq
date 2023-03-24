3fc530e1adc0b844bb274c90b2eafbc03720925d
IamMK <malk@int.pl>
2023-03-24 20:48:03 +0100

    Task3 completed, now using games parameter too

--- task3/index.php
+++ task3/index.php
@@ -4,25 +4,40 @@ class RankingTable
     public $users = null;
     public function recordResult($user, $points)
     {
-        foreach ($this->users as $item) {
+        foreach ($this->users as $index => $item) {
             if ($user == $item['username']) {
-                $item['score'] += $points;
-                echo $item['score'];
+                $this->users[$index]['score'] += $points;
+                $this->users[$index]['games']++;
                 return;
             }
         }
     }
 
+    public function playerRank($rank)
+    {
+        // in case of much bigger count of elements should be used quick sort
+        usort($this->users, function ($a, $b) {
+            $score = $b['score'] - $a['score'];
+            if ($score == 0)
+                $score = $a['games'] - $b['games'];
+            return $score;
+        });
+        return $this->users[$rank - 1]['username'];
+    }
+
     public function __construct($users)
     {
         foreach ($users as $user) {
-            $this->users[] = ['username' => $user, 'score' => 0];
+            $this->users[] = ['username' => $user, 'score' => 0, 'games' => 0];
         }
     }
 }
 
+
+
 $table = new RankingTable(array('Jan', 'Maks', 'Monika'));
+
 $table->recordResult('Jan', 2);
 $table->recordResult('Maks', 3);
 $table->recordResult('Monika', 5);
-// echo $table->playerRank(1); // Monika
+echo $table->playerRank(1);
