b46b2c75c59284d132609a800b63ad7e1fa26e8c
IamMK <malk@int.pl>
2023-03-26 15:58:54 +0200

    Result view

--- task2/index.php
+++ task2/index.php
@@ -21,3 +21,17 @@ class NumericInput extends TextInput
         }
     }
 }
+
+$text = new TextInput();
+$numeric = new NumericInput();
+
+$text->add('Some text');
+$text->add('S0me t3xt');
+
+$numeric->add('Some text');
+$numeric->add('S0me t3xt');
+$numeric->add('12345');
+
+print_r("Text Input: " . $text->getValue());
+echo '<br />';
+print_r("Numeric Input: " . $numeric->getValue());
